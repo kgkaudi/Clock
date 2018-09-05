643336e0ed6988b04d61c0ea32d44f759d8a3fa8
kgkaudi <konstantinos.gkaris@gmail.com>
2018-09-05 18:16:43 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.clock = new System.Windows.Forms.Label();
+            this.date = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SetAlarm = new System.Windows.Forms.Button();
+            this.CancelAlarm = new System.Windows.Forms.Button();
+            this.Message = new System.Windows.Forms.Label();
+            this.Status = new System.Windows.Forms.Label();
+            this.cityBox = new System.Windows.Forms.ComboBox();
+            this.worldTime = new System.Windows.Forms.Label();
+            this.worldClock = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // clock
+            // 
+            this.clock.AutoSize = true;
+            this.clock.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clock.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.clock.Location = new System.Drawing.Point(712, 9);
+            this.clock.Name = "clock";
+            this.clock.Size = new System.Drawing.Size(102, 39);
+            this.clock.TabIndex = 5;
+            this.clock.Text = "00:00";
+            // 
+            // date
+            // 
+            this.date.AutoSize = true;
+            this.date.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.date.Location = new System.Drawing.Point(12, 9);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(251, 39);
+            this.date.TabIndex = 6;
+            this.date.Text = "1 January 2018";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker1.Location = new System.Drawing.Point(719, 84);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(256, 45);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // SetAlarm
+            // 
+            this.SetAlarm.Location = new System.Drawing.Point(26, 84);
+            this.SetAlarm.Name = "SetAlarm";
+            this.SetAlarm.Size = new System.Drawing.Size(256, 45);
+            this.SetAlarm.TabIndex = 8;
+            this.SetAlarm.Text = "Set Alarm";
+            this.SetAlarm.UseVisualStyleBackColor = true;
+            this.SetAlarm.Click += new System.EventHandler(this.SetAlarm_Click);
+            // 
+            // CancelAlarm
+            // 
+            this.CancelAlarm.Location = new System.Drawing.Point(375, 84);
+            this.CancelAlarm.Name = "CancelAlarm";
+            this.CancelAlarm.Size = new System.Drawing.Size(256, 45);
+            this.CancelAlarm.TabIndex = 9;
+            this.CancelAlarm.Text = "Cancel/Stop Alarm";
+            this.CancelAlarm.UseVisualStyleBackColor = true;
+            this.CancelAlarm.Click += new System.EventHandler(this.CancelAlarm_Click);
+            // 
+            // Message
+            // 
+            this.Message.AutoSize = true;
+            this.Message.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Message.Location = new System.Drawing.Point(368, 257);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(0, 39);
+            this.Message.TabIndex = 10;
+            // 
+            // Status
+            // 
+            this.Status.AutoSize = true;
+            this.Status.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Status.Location = new System.Drawing.Point(12, 253);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(113, 39);
+            this.Status.TabIndex = 11;
+            this.Status.Text = "Status";
+            // 
+            // cityBox
+            // 
+            this.cityBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cityBox.FormattingEnabled = true;
+            this.cityBox.Items.AddRange(new object[] {
+            "Los Angeles, USA",
+            "New York, USA",
+            "London, UK",
+            "Paris, France",
+            "Berlin, Germany",
+            "Stockholm, Sweden",
+            "Athens, Greece",
+            "Moscow, Russia",
+            "Tehran, Iran",
+            "New Delhi, India",
+            "Beijing, China",
+            "Tokyo, Japan",
+            "Sydney, Australia"});
+            this.cityBox.Location = new System.Drawing.Point(375, 250);
+            this.cityBox.Name = "cityBox";
+            this.cityBox.Size = new System.Drawing.Size(600, 46);
+            this.cityBox.TabIndex = 12;
+            this.cityBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // worldTime
+            // 
+            this.worldTime.AutoSize = true;
+            this.worldTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.worldTime.Location = new System.Drawing.Point(368, 310);
+            this.worldTime.Name = "worldTime";
+            this.worldTime.Size = new System.Drawing.Size(0, 39);
+            this.worldTime.TabIndex = 14;
+            // 
+            // worldClock
+            // 
+            this.worldClock.AutoSize = true;
+            this.worldClock.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.worldClock.Location = new System.Drawing.Point(368, 184);
+            this.worldClock.Name = "worldClock";
+            this.worldClock.Size = new System.Drawing.Size(0, 39);
+            this.worldClock.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 450);
+            this.Controls.Add(this.worldClock);
+            this.Controls.Add(this.worldTime);
+            this.Controls.Add(this.cityBox);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.CancelAlarm);
+            this.Controls.Add(this.SetAlarm);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.clock);
+            this.Name = "Form1";
+            this.Text = "Digital Clock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label clock;
+        private System.Windows.Forms.Label date;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button SetAlarm;
+        private System.Windows.Forms.Button CancelAlarm;
+        private System.Windows.Forms.Label Message;
+        private System.Windows.Forms.Label Status;
+        private System.Windows.Forms.ComboBox cityBox;
+        private System.Windows.Forms.Label worldTime;
+        private System.Windows.Forms.Label worldClock;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            clock.Text = DateTime.Now.ToLongTimeString();
+            date.Text = DateTime.Now.ToLongDateString();
+
+            if (cityBox.Text == "Los Angeles, USA")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(-7).ToString();
+            }
+
+            if (cityBox.Text == "New York, USA")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(-4).ToString();
+            }
+
+            if (cityBox.Text == "London, UK")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+1).ToString();
+            }
+
+            if (cityBox.Text == "Paris, France")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+2).ToString();
+            }
+
+            if (cityBox.Text == "Berlin, Germany")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+2).ToString();
+            }
+
+            if (cityBox.Text == "Stockholm, Sweden")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+2).ToString();
+            }
+
+            if (cityBox.Text == "Athens, Greece")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+3).ToString();
+            }
+
+            if (cityBox.Text == "Moscow, Russia")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+3).ToString();
+            }
+
+            if (cityBox.Text == "Tehran, Iran")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+4.5).ToString();
+            }
+
+            if (cityBox.Text == "New Delhi, India")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+5.5).ToString();
+            }
+
+            if (cityBox.Text == "Beijing, China")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+8).ToString();
+            }
+
+            if (cityBox.Text == "Tokyo, Japan")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+9).ToString();
+            }
+
+            if (cityBox.Text == "Sydney, Australia")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+10).ToString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        public void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            DateTime currentTime = DateTime.Now;
+            DateTime userTime = dateTimePicker1.Value;
+            if(currentTime.Hour == userTime.Hour && currentTime.Minute == userTime.Minute && currentTime.Second == userTime.Second)
+            {
+                timer.Stop();
+                try
+                {
+                    SoundPlayer player = new SoundPlayer();
+                    player.SoundLocation = @"C:\Windows\media\Alarm01.wav";
+                    player.PlayLooping();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void SetAlarm_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+            Status.Text = "Set..";
+        }
+
+        private void CancelAlarm_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            SoundPlayer player = new SoundPlayer();
+            player.Stop();
+            Status.Text = "Stopped!!";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cityBox.Text == "Los Angeles, USA")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(-7).ToString();
+            }
+
+            if (cityBox.Text == "New York, USA")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(-4).ToString();
+            }
+            
+            if (cityBox.Text == "London, UK")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+1).ToString();
+            }
+            
+            if (cityBox.Text == "Paris, France")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+2).ToString();
+            }
+
+            if (cityBox.Text == "Berlin, Germany")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+2).ToString();
+            }
+
+            if (cityBox.Text == "Stockholm, Sweden")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+2).ToString();
+            }
+
+            if (cityBox.Text == "Athens, Greece")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+3).ToString();
+            }
+
+            if (cityBox.Text == "Moscow, Russia")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+3).ToString();
+            }
+
+            if (cityBox.Text == "Tehran, Iran")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+4.5).ToString();
+            }
+
+            if (cityBox.Text == "New Delhi, India")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+5.5).ToString();
+            }
+
+            if (cityBox.Text == "Beijing, China")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+8).ToString();
+            }
+
+            if (cityBox.Text == "Tokyo, Japan")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+9).ToString();
+            }
+
+            if (cityBox.Text == "Sydney, Australia")
+            {
+                worldClock.Text = DateTime.UtcNow.AddHours(+10).ToString();
+            }
+        }
+    }
+}
